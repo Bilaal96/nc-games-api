e68417a3e64c287c429cc6f377965238a7797457
Bilaal96 <bilaal-ahmad@hotmail.co.uk>
2023-03-30 15:39:31 +0100

    feat: GET /api/users - get array of all users

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -438,3 +438,24 @@ describe('DELETE /api/comments/:comments_id', () => {
       });
   });
 });
+
+describe('GET /api/users', () => {
+  test('200: returns an array of user objects', () => {
+    return request(app)
+      .get('/api/users')
+      .expect(200)
+      .then((response) => {
+        const { users } = response.body;
+        expect(users).toBeInstanceOf(Array);
+        expect(users).toHaveLength(4);
+
+        users.forEach((user) => {
+          expect(user).toMatchObject({
+            username: expect.any(String),
+            name: expect.any(String),
+            avatar_url: expect.any(String),
+          });
+        });
+      });
+  });
+});
